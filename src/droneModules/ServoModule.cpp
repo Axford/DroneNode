09245f7e3c4e64aa6f348c1a4c0bd16cd1a1fee2
Axford <Damian@axford.me.uk>
2021-10-21 23:30:29 +0100

    Update Servo for DCode

--- src/droneModules/ServoModule.cpp
+++ src/droneModules/ServoModule.cpp
@@ -7,9 +7,9 @@ ServoModule::ServoModule(uint8_t id, DroneModuleManager* dmm, DroneLinkManager* 
   DroneModule ( id, dmm, dlm, dem, fs )
  {
    setTypeName(FPSTR(SERVO_STR_SERVO));
-   _pins[0] = 0;
-   _limits[0] = -1;
-   _limits[1] = 1;
+   //_pins[0] = 0;
+   //_limits[0] = -1;
+   //_limits[1] = 1;
 
    // subs
    initSubs(SERVO_SUBS);
@@ -20,43 +20,61 @@ ServoModule::ServoModule(uint8_t id, DroneModuleManager* dmm, DroneLinkManager* 
    sub->addrParam = SERVO_SUB_POSITION_ADDR;
    sub->param.param = SERVO_SUB_POSITION;
    setParamName(FPSTR(STRING_POSITION), &sub->param);
+
+   // pubs
+   initParams(SERVO_PARAM_ENTRIES);
+
+   DRONE_PARAM_ENTRY *param;
+
+   param = &_params[SERVO_PARAM_PINS_E];
+   param->param = SERVO_PARAM_PINS;
+   setParamName(FPSTR(STRING_PINS), param);
+   param->paramTypeLength = _mgmtMsg.packParamLength(true, DRONE_LINK_MSG_TYPE_UINT8_T, 3);
+   _params[SERVO_PARAM_PINS_E].data.uint8[0] = 0;
+
+   param = &_params[SERVO_PARAM_LIMITS_E];
+   param->param = SERVO_PARAM_LIMITS;
+   setParamName(FPSTR(STRING_LIMITS), param);
+   param->paramTypeLength = _mgmtMsg.packParamLength(true, DRONE_LINK_MSG_TYPE_FLOAT, 8);
+   _params[SERVO_PARAM_LIMITS_E].data.f[0] = -1;
+   _params[SERVO_PARAM_LIMITS_E].data.f[1] = 1;
 }
 
 
-/*
-void ServoModule::loadConfiguration(JsonObject &obj) {
-  DroneModule::loadConfiguration(obj);
-
-  // limits
-  if (obj.containsKey(STRING_LIMITS)) {
-    Log.noticeln(F("[ServoModule.loadConfiguration]  Read limits..."));
-    JsonArray array = obj[STRING_LIMITS].as<JsonArray>();
-    uint8_t i=0;
-    for(JsonVariant v : array) {
-      if (i < sizeof(_limits))
-        _limits[i] = v | _limits[i];
-      i++;
-    }
-  }
-
-  //TODO: fix
-  //DroneModule::parsePins(obj, _pins, (uint8_t)sizeof(_pins));
+DEM_NAMESPACE* ServoModule::registerNamespace(DroneExecutionManager *dem) {
+  // namespace for module type
+  return dem->createNamespace(SERVO_STR_SERVO,0,true);
 }
-*/
+
+void ServoModule::registerParams(DEM_NAMESPACE* ns, DroneExecutionManager *dem) {
+  using std::placeholders::_1;
+  using std::placeholders::_2;
+  using std::placeholders::_3;
+  using std::placeholders::_4;
+
+  // writable mgmt params
+  DEMCommandHandler ph = std::bind(&DroneExecutionManager::mod_param, dem, _1, _2, _3, _4);
+  DEMCommandHandler pha = std::bind(&DroneExecutionManager::mod_subAddr, dem, _1, _2, _3, _4);
+
+  dem->registerCommand(ns, STRING_POSITION, DRONE_LINK_MSG_TYPE_FLOAT, ph);
+  dem->registerCommand(ns, PSTR("$position"), DRONE_LINK_MSG_TYPE_FLOAT, pha);
+  dem->registerCommand(ns, STRING_PINS, DRONE_LINK_MSG_TYPE_UINT8_T, ph);
+  dem->registerCommand(ns, STRING_LIMITS, DRONE_LINK_MSG_TYPE_FLOAT, ph);
+}
 
 
 void ServoModule::setup() {
   DroneModule::setup();
 
-  if (_pins[0] > 0) {
+  if (_params[SERVO_PARAM_PINS_E].data.uint8[0] > 0) {
     _servo.setPeriodHertz(50);// Standard 50hz servo
-    _servo.attach(_pins[0], 500, 2400);
+    _servo.attach(_params[SERVO_PARAM_PINS_E].data.uint8[0], 500, 2400);
 
     // init servo
     update();
 
   } else {
-    Log.errorln(F("Undefined pin %d"), _pins[0]);
+    Log.errorln(F("Undefined pin %d"), _params[SERVO_PARAM_PINS_E].data.uint8[0]);
     disable();
   }
 }
@@ -72,7 +90,7 @@ void ServoModule::loop() {
 
 
 void ServoModule::update() {
-  if (!_setupDone) return;
+  if (_error > 0 || !_setupDone) return;
 
   float v = _subs[SERVO_SUB_POSITION_E].param.data.f[0];
   // limit range
@@ -80,7 +98,7 @@ void ServoModule::update() {
   if (v< -1) v = -1;
 
   // remap -1 to 1 into _limits[0] to _limits[1]
-  v = (v + 1) * (_limits[1] - _limits[0]) / (2) + _limits[0];
+  v = (v + 1) * (_params[SERVO_PARAM_LIMITS_E].data.f[1] - _params[SERVO_PARAM_LIMITS_E].data.f[0]) / (2) + _params[SERVO_PARAM_LIMITS_E].data.f[0];
 
   int pos = (v*90.0) + 90.0;
   // limits
--- src/droneModules/ServoModule.h
+++ src/droneModules/ServoModule.h
@@ -9,7 +9,18 @@ Manages a PWM servo channel
 #include "../DroneModule.h"
 #include <ESP32Servo.h>
 
+// params
 
+#define SERVO_PARAM_PINS         10
+#define SERVO_PARAM_PINS_E       0
+
+#define SERVO_PARAM_LIMITS       11
+#define SERVO_PARAM_LIMITS_E     1
+
+#define SERVO_PARAM_ENTRIES      2
+
+
+// subs
 #define SERVO_SUB_POSITION         8
 #define SERVO_SUB_POSITION_ADDR    9
 #define SERVO_SUB_POSITION_E       0
@@ -21,15 +32,18 @@ static const char SERVO_STR_SERVO[] PROGMEM = "Servo";
 
 class ServoModule:  public DroneModule {
 protected:
-  uint8_t _pins[1];
+  //uint8_t _pins[1];
   Servo _servo;
-  float _limits[2];  // min, max
+  //float _limits[2];  // min, max
 public:
 
   ServoModule(uint8_t id, DroneModuleManager* dmm, DroneLinkManager* dlm, DroneExecutionManager* dem, fs::FS &fs);
 
-  virtual void setup();
-  virtual void loop();
+  static DEM_NAMESPACE* registerNamespace(DroneExecutionManager *dem);
+  static void registerParams(DEM_NAMESPACE* ns, DroneExecutionManager *dem);
+
+  void setup();
+  void loop();
 
   void update();
 
