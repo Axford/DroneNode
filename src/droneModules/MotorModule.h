dd77e3044f1efb23c7e3575b194d63887cd48c7d
Axford <Damian@axford.me.uk>
2022-01-09 20:59:28 +0000

    Help info for Motor module

--- src/droneModules/MotorModule.h
+++ src/droneModules/MotorModule.h
@@ -1,6 +1,12 @@
 /*
 
-Manages a PWM MOTOR channel
+@type          Motor
+@inherits      Drone
+@description   Manages a Motor via an H Bridge module using PWM
+
+@guide >>>
+todo
+<<<
 
 */
 #ifndef MOTOR_MODULE_H
@@ -9,23 +15,28 @@ Manages a PWM MOTOR channel
 #include "../DroneModule.h"
 
 
-// params
+// pubs
 
+// @pub 10;u8;3;pins;Pin connections for the H Bridge (A, B, EN)
 #define MOTOR_PARAM_PINS         10
 #define MOTOR_PARAM_PINS_E       0
 
+// @pub 11;u8;1;PWMChannel;Which ESP32 PWM channel to use (default 15)
 #define MOTOR_PARAM_PWMCHANNEL   11
 #define MOTOR_PARAM_PWMCHANNEL_E 1
 
+// @pub 12;f;2;limits;Min and max speed limits in range -1 to 1 (default -1 1)
 #define MOTOR_PARAM_LIMITS       12
 #define MOTOR_PARAM_LIMITS_E     2
 
+// @pub 13;f;1;deadband;Input range within which to set the motor output to zero, interpreted abs(input).  Default 0.3
 #define MOTOR_PARAM_DEADBAND     13
 #define MOTOR_PARAM_DEADBAND_E   3
 
 #define MOTOR_PARAM_ENTRIES      4
 
 // subs
+// @sub 8;9;f;1;speed;Desired Motor speed in range -1 to 1
 #define MOTOR_SUB_SPEED         8
 #define MOTOR_SUB_SPEED_ADDR    9
 #define MOTOR_SUB_SPEED_E       0
